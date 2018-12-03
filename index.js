be85eaf16fdd8849bfcce5327eb90ed9de3af41e
jakub <jakub.polaczek@netguru.co>
2018-12-03 19:26:59 +0100

    controllers implemented

--- index.js
+++ index.js
@@ -1,76 +1,37 @@
-const express = require('express');
-const path = require('path');
-const expressEdge = require('express-edge');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const Post = require('./database/models/Post');
-
+const expressEdge = require("express-edge");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 
-const createPost = require('./middleware/createPost')
+const newPostController = require('./controllers/newPost')
+const homePageController = require('./controllers/homePage')
+const createPostController = require('./controllers/createPost')
+const getPostController = require('./controllers/getPost')
 
 const app = new express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/blog', { useNewUrlParser: true })
+mongoose.connect('mongodb://127.0.0.1:27017/node-blog', { useNewUrlParser: true })
     .then(() => 'You are now connected to Mongo!')
     .catch(err => console.error('Something went wrong', err))
 
-
-app.use(express.static('public'));
 app.use(fileUpload());
+app.use(express.static("public"));
 app.use(expressEdge);
 app.set('views', __dirname + '/views');
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/posts/create', createPost)
+const storePost = require('./middleware/createPost')
 
-app.get('/', async (req, res) => {
-    const posts = await Post.find({})
-    res.render('index', {
-        posts
-    })
-});
+app.use('/posts/store', storePost)
 
-app.get('/about', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
-});
-
-app.get('/contact', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
-});
-
-app.get('/post', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'));
-});
-
-app.get('/posts/new', (req, res) => {
-    res.render('new')
-});
-
-app.post('/posts/create', (req, res) => {
-  const { image } = req.files
-
-   image.mv(path.resolve(__dirname, 'public/posts', image.name), (error) => {
-       Post.create({
-           ...req.body,
-           image: `/posts/${image.name}`
-       }, (error, post) => {
-           res.redirect('/');
-       });
-   })
-});
-
-app.get('/post/:id', async (req, res) => {
-    const post = await Post.findById(req.params.id)
-    res.render('post', {
-        post
-    })
-});
+app.get("/", homePageController);
+app.get("/post/:id", getPostController);
+app.get("/posts/new", newPostController);
+app.post("/posts/create", createPostController);
 
 app.listen(4000, () => {
-    console.log('App listening on port 4000')
+  console.log("App listening on port 4000");
 });
